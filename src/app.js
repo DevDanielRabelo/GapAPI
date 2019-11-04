fc6471d19946af3b0a9f3ce307457c962e4633c7
DevDanielRabelo <danielrabelo2000@gmail.com>
2019-11-04 00:55:29 -0300

    connect banco

--- src/app.js
+++ src/app.js
@@ -2,8 +2,14 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config');
 
 const app = express();
+
+//Conect ao banco
+mongoose.connect(config.connectionString);
+
 
 //Carregar as Rotas
 const indexRoute = require('./routes/index-route');
@@ -14,7 +20,7 @@ app.use(bodyParser.json({
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Habilita o CORS
-app.use(function (req, res, next){
+app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
---
+++ src/config.js
@@ -0,0 +1,10 @@
+global.SALT_KEY = '';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo ao Teste Node.js';
+
+module.exports = {
+    connectionString: 'mongodb+srv://gap1:ZJUnoAAHhkEJyz8p@cluster0-r6m5y.azure.mongodb.net/test?retryWrites=true&w=majority',
+    sendgridkey: '',
+    containerConnectionString: ''
+}
+
+
