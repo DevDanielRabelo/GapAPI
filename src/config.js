684cb00956a29c1b657fb17b1a62029e2695148d
DevDanielRabelo <danielrabelo2000@gmail.com>
2019-11-04 02:03:10 -0300

    new connect

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = '';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo ao Teste Node.js';
 
 module.exports = {
-    connectionString: 'mongodb+srv://gap1:ZJUnoAAHhkEJyz8p@cluster0-r6m5y.azure.mongodb.net/test?retryWrites=true&w=majority',
+    connectionString: 'mongodb+srv://gap:MLEesBvtpw9mdSoF@cluster0-p9gfe.azure.mongodb.net/test',
     sendgridkey: '',
     containerConnectionString: ''
 }
