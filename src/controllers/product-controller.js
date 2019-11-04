90680e6b4a7f4d20496ec2a5ff363d1200c28b2c
DevDanielRabelo <danielrabelo2000@gmail.com>
2019-11-04 02:28:41 -0300

    teste

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,17 +1,34 @@
 'use strict'
 
 const repository = require('../repositories/produto-repository');
+const mongoose = require("mongoose");
+const Product = mongoose.model('Produto');
 
-exports.get = async (req, res, next) => {
-    try {
-        var data = await repository.get();
-        res.status(200).send(data);
-    } catch (e) {
-        res.status(500).send({
-            massage: 'Falha ao processar sua requisição'
-        });
-    }
+// exports.get = async (req, res, next) => {
+//     try {
+//         var data = await repository.get();
+//         res.status(200).send(data);
+//     } catch (e) {
+//         res.status(500).send({
+//             massage: 'Falha ao processar sua requisição'
+//         });
+//     }
+// };
+
+
+exports.get = (req, res, next) => {
+    Product
+        .find({}).then(x=>{
+            res.status(200).send({
+                data: x
+            })
+        }).catch(e =>{
+            res.status(400).send('erro')
+        })
 };
+
+
+
 
 exports.getBySlug = async (req, res, next) => {
     try {
