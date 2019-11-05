b129a217053cf1a038bd0cce3f24c849b2a07ddc
DevDanielRabelo <danielrabelo2000@gmail.com>
2019-11-04 22:40:27 -0300

    getBySlug/getByTag produto

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -16,24 +16,24 @@ exports.get = async (req, res, next) => {
 };
 
 
-// exports.getBySlug = async (req, res, next) => {
-//     try {
-//         var data = await repository.getBySlug(req.params.slug);
-//         res.status(200).send(data);
-//     } catch (e) {
-//         res.status(500).send({
-//             massage: 'Falha ao processar sua requisição'
-//         });
-//     }
-// };
+exports.getBySlug = async (req, res, next) => {
+    try {
+        var data = await repository.getBySlug(req.params.slug);
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(500).send({
+            massage: 'Falha ao processar sua requisição'
+        });
+    }
+};
 
-// exports.getByTag = async (req, res, next) => {
-//     try {
-//         var data = await repository.getByTag(req.params.tag);
-//         res.status(200).send(data);
-//     } catch (e) {
-//         res.status(500).send({
-//             massage: 'Falha ao processar sua requisição'
-//         });
-//     }
-// };
+exports.getByTag = async (req, res, next) => {
+    try {
+        var data = await repository.getByTag(req.params.tag);
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(500).send({
+            massage: 'Falha ao processar sua requisição'
+        });
+    }
+};
