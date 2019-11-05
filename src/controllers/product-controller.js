684738cbc5a6869f251dfc43371aafaff1750768
DevDanielRabelo <danielrabelo2000@gmail.com>
2019-11-04 22:29:08 -0300

    up teste

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -4,26 +4,16 @@ const repository = require('../repositories/produto-repository');
 const mongoose = require("mongoose");
 const Product = mongoose.model('Produto');
 
-// exports.get = async (req, res, next) => {
-//     try {
-//         var data = await repository.get();
-//         res.status(200).send(data);
-//     } catch (e) {
-//         res.status(500).send({
-//             massage: 'Falha ao processar sua requisição'
-//         });
-//     }
-// };
-
-exports.get = (req, res, next) => {
-        Product.find({}).then(x=>{
-            res.status(200).send(x);
-        }).catch(e => {
-            res.status(400).send(e);
-        })
-    
+exports.get = async (req, res, next) => {
+    try {
+        var data = await repository.get();
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(500).send({
+            massage: 'Falha ao processar sua requisição'
+        });
+    }
 };
-
 
 
 // exports.getBySlug = async (req, res, next) => {
