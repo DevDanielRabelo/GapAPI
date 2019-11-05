967bdd6cec111084907617d66d6a6cd5d3b52d81
DevDanielRabelo <danielrabelo2000@gmail.com>
2019-11-04 22:35:55 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -2,17 +2,17 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const config = require('./config');
 
 const app = express();
 
 //Conect ao banco
-// mongoose.connect('mongodb://gap-bd:tV95FEuTXywlezOrQbpR02KcS9AgVdCJaZHVzh8X3NYvUU3Hyl47xZWaI5snFnK5G7kd7mKPyeW0v8lqCxaKeg==@gap-bd.documents.azure.com:10255/?ssl=true');
-var mongoClient = require("mongodb").MongoClient;
-mongoClient.connect("mongodb://gap-bd:tV95FEuTXywlezOrQbpR02KcS9AgVdCJaZHVzh8X3NYvUU3Hyl47xZWaI5snFnK5G7kd7mKPyeW0v8lqCxaKeg==@gap-bd.documents.azure.com:10255/?ssl=true", function (err, db) {
-  db.close();
-});
+mongoose.connect('mongodb://gap-bd:tV95FEuTXywlezOrQbpR02KcS9AgVdCJaZHVzh8X3NYvUU3Hyl47xZWaI5snFnK5G7kd7mKPyeW0v8lqCxaKeg%3D%3D@gap-bd.documents.azure.com:10255/?ssl=true');
+// var mongoClient = require("mongodb").MongoClient;
+// mongoClient.connect("mongodb://gap-bd:tV95FEuTXywlezOrQbpR02KcS9AgVdCJaZHVzh8X3NYvUU3Hyl47xZWaI5snFnK5G7kd7mKPyeW0v8lqCxaKeg==@gap-bd.documents.azure.com:10255/?ssl=true", function (err, db) {
+//   db.close();
+// });
 
 //Carregar os Models
 const Product = require('./models/produto');
