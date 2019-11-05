868b1834fd12fef6d22703024caf63ad519587d8
DevDanielRabelo <danielrabelo2000@gmail.com>
2019-11-04 21:12:59 -0300

    teste produto

--- src/repositories/produto-repository.js
+++ src/repositories/produto-repository.js
@@ -4,25 +4,28 @@ const mongoose = require("mongoose");
 const Product = mongoose.model('Produto');
 
 exports.get = async () => {
-    const res = await Product
-        .find({
-            active: true
-        }, 'nome preco slug');
+    // const res = await Product
+    //     .find({
+    //         active: true
+    //     }, 'nome preco slug');
+    const res = {
+        produto: 'novo produto'
+    }
     return res;
 }
 
-exports.getBySlug = async (slug) => {
-    const res = await Product.findOne({
-        slug: slug,
-        active: true
-    }, 'nome descricao preco slug tags');
-    return res;
-}
+// exports.getBySlug = async (slug) => {
+//     const res = await Product.findOne({
+//         slug: slug,
+//         active: true
+//     }, 'nome descricao preco slug tags');
+//     return res;
+// }
 
-exports.getByTag = async (tag) => {
-    const res = await Product.find({
-        tags: tag,
-        active: true
-    }, 'nome descricao preco slug tags');
-    return res;
-}
+// exports.getByTag = async (tag) => {
+//     const res = await Product.find({
+//         tags: tag,
+//         active: true
+//     }, 'nome descricao preco slug tags');
+//     return res;
+// }
