bc7d486e3474ae3e1b891768a49d1676ad1a61e3
DevDanielRabelo <danielrabelo2000@gmail.com>
2019-11-05 21:31:37 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const config = require('./config');
+const debug = require('debug');
 
 const app = express();
 
